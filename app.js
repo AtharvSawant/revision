e5e20cdff3b38ca9844a76accd867866c7cdc580
AtharvSawant <96947605+AtharvSawant@users.noreply.github.com>
2022-01-20 20:45:40 +0530

    Added file

---
+++ app.js
@@ -0,0 +1,58 @@
+var x =13;
+var y = 25.8;
+console.log(x);
+var name =DaddyCalling
+console.log(name+x);
+var z;
+
+function random(){
+    z=Math.random();
+    document.getElementById("random").innerHTML=z;
+}
+
+function round(){
+    z=Math.round(y);
+    document.getElementById("round").innerHTML=z;
+}
+
+function ceil(){
+    z=Math.ceil(y);
+    document.getElementById("ceil").innerHTML=z;
+}
+
+function floor(){
+    z=Math.floor(y);
+    document.getElementById("floor").innerHTML=z;
+}
+
+function pow(){
+    z=Math.pow(2,7);
+    document.getElementById("pow").innerHTML=z;
+}
+
+function sqrt(){
+    z=Math.sqrt(100);
+    document.getElementById("sqrt").innerHTML=z;
+}
+
+function PI(){
+    z=Math.PI;
+    document.getElementById("PI").innerHTML=z;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
